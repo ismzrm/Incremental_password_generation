e7a9306f31f95dbcdb5f91013f9602385874634e
ismail <153764937+ismzrm@users.noreply.github.com>
2024-12-16 13:31:17 +0300

    Incremental password generation
    Incremental password generation

---
+++ 2024.11.18/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace _2024._11._18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ŞİFRE OLUŞTUR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 155);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 225);
+            this.listBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "kolay şifre",
+            "orta şifre ",
+            "zor şifre"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 128);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(240, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(264, 429);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+
+    }
+}
+
---
+++ 2024.11.18/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2024._11._18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static string sifre_yarat(int seviye)
+        {
+            string sifre="";
+            char[] dizi = "QWERTYUIOPĞÜASDFGHJKLŞİZXCVBNMÖÇ".ToArray() ;
+            //dizide tochararray ve toarray aynı sonucu verir
+            Random rasgele = new Random();
+            
+
+            for (int i = 1; i <= seviye; i++)
+            { 
+            sifre +=dizi[rasgele.Next(dizi.Count())] ;
+
+            }
+
+
+
+                return sifre;
+        }
+        int seviye;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("LÜTFEN SEÇİM YAPINIZ!!!");
+                return;
+            }
+            if (comboBox1.SelectedIndex == 0) {seviye=4 ;}
+            if (comboBox1.SelectedIndex == 1) {seviye=8 ;}
+            if (comboBox1.SelectedIndex == 2) {seviye=12 ;}
+
+
+
+            label1.Text = sifre_yarat(seviye);
+            foreach (string benzer in listBox1.Items)
+            {
+                if (benzer == sifre_yarat(seviye))
+                {
+                    MessageBox.Show("Oluşturulan şifre zaten var");
+                    return;
+                }
+            }
+            listBox1.Items.Add(sifre_yarat(seviye));
+        }
+    }
+}
